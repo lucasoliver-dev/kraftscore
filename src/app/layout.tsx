81a99931a6b3b1740aad9415fe0704cb1106be36
Lucas Oliver <lucasoliver-dev@outlook.com>
2026-02-11 15:18:10 -0300

    feat/image-bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,32 @@ import AppBackground from '@/components/layout/AppBackground'
 import { satoshi, spaceGrotesk } from '@/styles/fonts'
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://kraftscore.com.br'),
   description: 'Sports betting and AI analysis application',
   title: 'KraftScore Insights',
+
+  openGraph: {
+    title: 'KraftScore Insights',
+    description: 'Sports betting and AI analysis application',
+    url: 'https://kraftscore.com.br',
+    siteName: 'KraftScore Insights',
+    images: [
+      {
+        url: '/public/images/ksInsights-bg.png',
+        width: 1200,
+        height: 630,
+        alt: 'KraftScore Insights',
+      },
+    ],
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'KraftScore Insights',
+    description: 'Sports betting and AI analysis application',
+    images: ['/public/images/ksInsights-bg.png'],
+  },
 }
 
 /**
